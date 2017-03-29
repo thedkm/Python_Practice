4bb89455fd751babf92f912215ad67ec993bd76f
Dinesh Mali DKm <dkmsn8@gmail.com>
2017-03-29 14:29:36 +0530

    Added comment

--- Searching Algorithms/linear_search.py
+++ Searching Algorithms/linear_search.py
@@ -1,3 +1,4 @@
+#Time complexity O(n)
 
 def sequential_search(data,target,low,high):
 	for i in range(low,high):
@@ -5,11 +6,7 @@ def sequential_search(data,target,low,high):
 			print("data is found at index {} and data is {}".format(i,data[i])) 
 			break
 		else:print("data is not found! !!")
-
 			
-
-
-
 def main():
     
     data=[2, 4 ,5, 7, 8 ,9,12, 14 ,17, 19 ,22 ,25, 27 ,28, 33,37]
@@ -19,10 +16,6 @@ def main():
 
    
     sequential_search(data,target,low,high)
-
-
-
-
 
 
 if __name__ == '__main__': main()
